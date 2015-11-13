421c3069f30da2c81738365ea4044f824e790681
Griffin Gordon Scott <gscott@basebox.saintjoe.edu>
2015-11-13 14:32:38 -0600

    This is the first commit for the Character Creator Program, now written in Java.

---
+++ CharacterCreator.java
@@ -0,0 +1,7 @@
+/* This is the Character Creator Program made by Griffin Scott for CMP 343: Software Engineering.
+Origionally this program was to be written in JavaScript until I came to the realization that
+JavaScript was the wrong programming language for this type of program.
+
+Today's Data: 11/13
+
+*PLACE HOLDER FOR CITING RESOURCES* */
