1f977e10f106df602cb5babba7a842cd61f30914
Griffin Gordon Scott <gscott@basebox.saintjoe.edu>
2015-11-15 17:48:55 -0600

    Added more to the game selector class and commenting.

--- CharacterCreator.java
+++ CharacterCreator.java
@@ -3,23 +3,31 @@ Origionally this program was to be written in JavaScript until I came to the rea
 JavaScript was the wrong programming language for this type of program.
 
 Date: 11/15
-
-*GAME SYSTEM NUMBER RECORD*
+*/
+/*GAME SYSTEM NUMBER RECORD
 0: Nations: The RPG by Griffin Scott
 1: Pathfinder by Paizo Publishing
-2: EMPTY
+2: EMPTY */
 
-*PLACE HOLDER FOR CITING RESOURCES* */
+/* Resource Citing
+http://illegalargumentexception.blogspot.com/2010/09/java-systemconsole-ides-and-testing.html Provides examples of non-console programming.
+*/
 
 //GLOBAL VARIABLES
 
 //Functions
 public class CharacterCreator {
 
-  public static void main(String[] args){
+  public static void main(String[] args){ //This is where the magic happens. This is going to be really big.
 
   } //End Main
-  public static int gameSelector(int whatGame){
 
+  public static int gameSelector(int whatGame){ //This is where the user selects which game system they want to chose.
+    console.println("This is the Game Selection area. Please type the number of
+    the game system that you want to use. To see what game systems are avaliable,
+     please type Games."); //Prints the instructions for this part of the program.
+
+     //This will start the parsing. 
   }//End gameSelector
+
 } //End CharacterCreator
