a427c3eb324cb555fac587ac6653b2ee36cc324b
Griffin Gordon Scott <gscott@basebox.saintjoe.edu>
2015-11-15 17:04:55 -0600

    This is the next part of my revised CharacterCreator program. There's a bit more explaination to it, and I added the int to determine which game system will be active.

--- CharacterCreator.java
+++ CharacterCreator.java
@@ -4,4 +4,16 @@ JavaScript was the wrong programming language for this type of program.
 
 Today's Data: 11/13
 
+*GAME SYSTEM NUMBER RECORD*
+0: Nations: The RPG by Griffin Scott
+1: Pathfinder by Paizo Publishing
+2: EMPTY
+
 *PLACE HOLDER FOR CITING RESOURCES* */
+
+//GLOBAL VARIABLES
+int whatGame; //This contains the selected number for the game system. See the Game System Number Record for more info.
+
+public class GameSelection {
+  
+}
