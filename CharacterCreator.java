b0bc504f2ccf216ce0faca1679a927674f305ffe
Raiu1994 <gsv9565@saintjoe.edu>
2015-11-30 12:56:40 -0600

    Added an incomplete version of the Character Creation function. Hid commands that will come out in a future release once a save system is implemented.

--- CharacterCreator.java
+++ CharacterCreator.java
@@ -38,10 +38,10 @@ public class CharacterCreator {
 		  	case "Help":
 		  		System.out.println("The following commands are avaliable. "
 		  				+ "Game Selector: Allows selection of game system. "
-		  				+ "Load: Opens the Load Character Sheet Program. "
-		  				+ "Create Character: Creates a new character using the currently selected system. "
-		  				+ "Delete Character: Opens the Delete Character program.");
-		  		System.out.println("Currently, only Game Selector and Create Character commands are completed.");
+		  				//+ "Load: Opens the Load Character Sheet Program. "
+		  				+ "Create Character: Creates a new character using the currently selected system. ");
+		  				//+ "Delete Character: Opens the Delete Character program.");
+		  		//System.out.println("Currently, only Game Selector and Create Character commands are completed.");
 		  		break; 
 		  	case "Game Selector": //This will proc gameSelector, allowing the user to select which game system they want to use.
 		  		System.out.println("You have selected the Game Selector command. Please enter the first name of the game system you want, or enter List to see the list of game systems.");
@@ -49,7 +49,7 @@ public class CharacterCreator {
 		  		//System.out.println("Game system is: " + gameSystem);
 		  		//progress++;
 		  		break;
-		  	case "Load": //This will ask for the name of a character, in order to load it.
+		  	case "Load": //This will ask for the name of a character, in order to load it. This will be added once a save system is implemented.
 		  		//Insert Load command
 		  		//progress++;
 		  		break;
@@ -58,11 +58,11 @@ public class CharacterCreator {
 		  		System.out.println("Please insert the name of the character you want to create.");
 		  		String characterName = name.nextLine();
 		  		System.out.println("Creating a character with the name : " +characterName);
-		  		//Put in Character Creator command.
+		  		characterCreation(characterName);
 		  		break;
-		  	case "Delete Character": //Asks for the name of the character to remove.
-		  		//Insert the Delete command.
+		  	case "Delete Character": //Asks for the name of the character to remove. This will be implemented with a save system.
 		  		System.out.println("Please insert the name of the character that you want to delete.");
+		  		//Insert the Delete command.
 		  		//progress++;
 		  		break;
 		  	default: System.out.println("I'm sorry, the command you entered was not valid. If you need help with the commands, please enter Help. Capitalization is required.");
@@ -87,7 +87,7 @@ public class CharacterCreator {
     		  		gameSystem = 1; //Sets gameSystem to Nations the RPG.
     		  		break;
     		  	case "Pathfinder": System.out.println("You have selected Pathfinder. "
-    		  			+ "WARNING this game system is incomplete.");
+    		  			+ "WARNING this game system is incomplete and serves as a test function for dice rolling.");
     		  		gameSystem = 2; //Sets gameSystem to Pathfinder.
     		  		break;
     		  	default: System.out.println("I'm sorry, but that is not a valid command. For a list of Game Systems, please enter the word 'List'.");
@@ -100,6 +100,30 @@ public class CharacterCreator {
       return gameSystem; //Sends back the chosen game system to Main.
   }//End gameSelector
   
+  public static void characterCreation (String characterName){
+	  String[] nameArray; //Holds the Player Name, Character Name, Game System, and any background info.
+	  int[] statArray; //Holds the stat line.
+	  Scanner c = new Scanner(System.in);
+	  
+	  int progress; //This keeps the loop going until character creation is completed.
+	  
+	  progress = 0; //No progress so far.
+	  
+	  while (progress == 0){
+		  if (gameSystem == 0){ //Catches anyone who tries to do this out of order.
+			  System.out.println("The Game System must be selected before a character can be created. Please restart the program and select a game system using the Game System command.");
+			  break;
+		  }
+		  if (gameSystem == 1){
+			  System.out.println("You have created a new nation called: " +characterName);
+		  }
+		  if (gameSystem == 2){
+			  System.out.println("You have created a new adventurer called: " +characterName);
+			  System.out.println("Warning, the Pathfinder option is not yet complete, as the game is much more expansive than can be covered in a single semester's worth of work. Currently, this serves only as a methoid to test the dice function.");
+			  
+		  }
+	  }
+  }
   /*New public method, called Creation. Creates an array based on the requirements of each game system. gameSystem will be sent to this method, which will cycle through an if statement. If gameSystem == 1, then Nations. If gameSystem == 2, then Pathfinder.
   For Nations: Player Name, Origin, Origin Sub-type, Military, Economy, Diplomacy, Modified Military, Modified Economy, Modified Diplomacy, Government Type, Nation Name. Create 2 arrays for Powers and Historical Importances that change based on how many Powers/HI are given by their chosen info.
   For Pathfinder: Player Name, Character Name, Level. Look to PFSRD for full list.
@@ -108,6 +132,7 @@ public class CharacterCreator {
   
   Pull data from the ArrayList program, including the dynamic sizing.
   */
+  
   
   
   
